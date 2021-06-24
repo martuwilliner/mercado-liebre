137ccc0d262767b954d060990d13bb0426672b71
martuwilliner <williner.martin@gmail.com>
2021-06-23 21:17:45 -0300

    terminado el register(html, css) y agregué el login(html, css)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# mercado-liebre
+# mercado-liebre 
+Proyecto personal, a modo de prueba, para Digital House.
--- src/routes/web.js
+++ src/routes/web.js
@@ -5,6 +5,13 @@ const path = require ("path");
 //Website Routes
 app.get("/", (req,res) => res.sendFile(path.resolve(__dirname, "../views/index.html")));
 
+//Routes Register
+app.get("/register",(req,res) => res.sendFile(path.resolve(__dirname,"../views/register.html")));
+
+// Routes Login
+app.get("/login",(req,res) => res.sendFile(path.resolve(__dirname,"../views/login.html")));
+
+
 module.exports = app;
 
 
